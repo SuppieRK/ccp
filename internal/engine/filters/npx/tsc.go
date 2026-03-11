c829b619ebc80e3bf489cc1975f14b3568221c53
Roman Khlebnov <suppie.rk@gmail.com>
2026-03-11 21:08:13 +0100

    fix: Adding TSC filter (#47)
    * fix: Adding TSC filter
    
    * fix: Adding TSC filter

--- internal/engine/filters/npx/tsc.go
+++ internal/engine/filters/npx/tsc.go
@@ -1,11 +1,10 @@
 package npxfilters
 
 import (
-	"regexp"
-	"sort"
 	"strings"
 
 	"go-command-compression-proxy/internal/engine"
+	filtercommon "go-command-compression-proxy/internal/engine/filters/common"
 )
 
 func NewNpxTscFilter() engine.ToolFilter { return tscFilter{} }
@@ -19,7 +18,7 @@ func (tscFilter) MaskingHorizon() int {
 }
 func (tscFilter) Prepare(args []string) engine.PrepareResult {
 	normalized := append([]string{}, args...)
-	if !hasPrettyFlag(normalized) {
+	if _, specified := filtercommon.TSCPrettyMode(normalized); !specified {
 		normalized = append(normalized, "--pretty", "false")
 	}
 	return engine.PrepareResult{NormalizedArgs: normalized}
@@ -62,86 +61,6 @@ func processRouted(ev engine.Event, mem *engine.OrderedSetBuffer) engine.Decisio
 		return engine.Decision{Action: engine.ActionCollect}
 	}
 }
-
-var tscDiagRe = regexp.MustCompile(`^(.+?)\((\d+),(\d+)\):\s+(error|warning)\s+(TS\d+):\s+(.+)$`)
-
-func hasPrettyFlag(args []string) bool {
-	for _, arg := range args {
-		a := strings.TrimSpace(strings.ToLower(arg))
-		if a == "--pretty" || strings.HasPrefix(a, "--pretty=") {
-			return true
-		}
-	}
-	return false
-}
-
 func summarizeTSCOutput(raw string) (string, bool) {
-	type diag struct {
-		file     string
-		line     string
-		col      string
-		severity string
-		code     string
-		msg      string
-	}
-	diags := make([]diag, 0, 32)
-	for _, line := range strings.Split(raw, "\n") {
-		trimmed := strings.TrimSpace(line)
-		if trimmed == "" {
-			continue
-		}
-
-		m := tscDiagRe.FindStringSubmatch(trimmed)
-		if len(m) != 7 {
-			continue
-		}
-
-		diags = append(diags, diag{
-			file:     m[1],
-			line:     m[2],
-			col:      m[3],
-			severity: m[4],
-			code:     m[5],
-			msg:      m[6],
-		})
-	}
-
-	if len(diags) == 0 {
-		return "", false
-	}
-
-	sort.Slice(diags, func(i, j int) bool {
-		if diags[i].file != diags[j].file {
-			return diags[i].file < diags[j].file
-		}
-		if diags[i].line != diags[j].line {
-			return diags[i].line < diags[j].line
-		}
-		if diags[i].col != diags[j].col {
-			return diags[i].col < diags[j].col
-		}
-		return diags[i].code < diags[j].code
-	})
-
-	var b strings.Builder
-	lastFile := ""
-	for _, d := range diags {
-		if d.file != lastFile {
-			b.WriteString(d.file)
-			b.WriteString(":\n")
-			lastFile = d.file
-		}
-		b.WriteString("- ")
-		b.WriteString(d.line)
-		b.WriteString(":")
-		b.WriteString(d.col)
-		b.WriteString(" ")
-		b.WriteString(d.severity)
-		b.WriteString(" ")
-		b.WriteString(d.code)
-		b.WriteString(" ")
-		b.WriteString(d.msg)
-		b.WriteString("\n")
-	}
-	return b.String(), true
+	return filtercommon.SummarizeTSCOutput(raw)
 }
